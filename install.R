890d1784c6d7104738bb9e613e7f6f0632ccbfd0
C Reid <charlesreid1@Gmail.com>
2019-05-09 15:23:13 -0700

    replace binder-example/r install.R with empty install.R from master branch

--- install.R
+++ install.R
@@ -1,3 +1,19 @@
-pkgs = c("tidyverse", "rmarkdown", "httr", "shinydashboard", "leaflet")
-ncores = parallel::detectCores()
-install.packages(pkgs, Ncpus = ncores)
+### install regular packages
+### reticulate: python support in r markdown
+### ggplot2: plotting
+### rmarkdown: knitting
+### caTools: knitting
+### bitops: knitting
+#pkgs = c("reticulate", "ggplot", "rmarkdown", "caTools", "bitops")
+#ncores = parallel::detectCores()
+#install.packages(pkgs, Ncpus = ncores)
+
+### install bioconductor packages
+#pkgs = c("BiocManager", Ncpus = ncores)
+#install.packages(pkgs, Ncpus = ncores)
+#BiocManager::install("package")
+
+### install GitHub packages (tag = commit, branch or release tag)
+#pkgs = c("devtools")
+#install.packages(pkgs, Ncpus = ncores)
+#devtools::install_github("user/repo", ref="tag")
