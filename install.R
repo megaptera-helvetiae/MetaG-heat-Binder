fc6caea5a85f715a4e29d94917eeb330a6268ccb
C Reid <charlesreid1@Gmail.com>
2019-05-09 16:34:48 -0700

    add 3 packages to install.R

--- install.R
+++ install.R
@@ -1,19 +1,3 @@
-### install regular packages
-### reticulate: python support in r markdown
-### ggplot2: plotting
-### rmarkdown: knitting
-### caTools: knitting
-### bitops: knitting
-#pkgs = c("reticulate", "ggplot", "rmarkdown", "caTools", "bitops")
-#ncores = parallel::detectCores()
-#install.packages(pkgs, Ncpus = ncores)
-
-### install bioconductor packages
-#pkgs = c("BiocManager", Ncpus = ncores)
-#install.packages(pkgs, Ncpus = ncores)
-#BiocManager::install("package")
-
-### install GitHub packages (tag = commit, branch or release tag)
-#pkgs = c("devtools")
-#install.packages(pkgs, Ncpus = ncores)
-#devtools::install_github("user/repo", ref="tag")
+pkgs = c("magrittr", "gplots", "tibble")
+ncores = parallel::detectCores()
+install.packages(pkgs, Ncpus = ncores)
